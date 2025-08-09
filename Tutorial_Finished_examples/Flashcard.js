4ddff247a17f3a33b9f01f2f6857cbe647c699a1
LucasJohnson <woodsman.lucas@gmail.com>
2025-08-09 10:43:19 -0700

    Finished Flashcard tut

--- Tutorial_Finished_examples/Flashcard.js
+++ Tutorial_Finished_examples/Flashcard.js
@@ -1,4 +1,16 @@
+let faceup = true;
+
 function flipCard() {
-  document.getElementById("Flashcard").innerHTML = "How to save a life";
+  if(faceup){
+    document.getElementById("Flashcard").innerHTML = "How to save a life";
+    faceup = false;  
+  }else{
+    document.getElementById("Flashcard").innerHTML = "What is the first album by the Fray";
+    faceup = true
+  }
+}
+
+function giveHint() {
+  document.getElementById("hint").innerHTML = "Go to wikipedia and find the band The Fray";
 }
 
